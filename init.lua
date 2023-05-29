d9dd554b2a4f0551664195066f6e675b3387eff2
TPCAD <1814083420@qq.com>
2023-05-29 23:10:40 +0800

    lazy.nvim

--- init.lua
+++ init.lua
@@ -1,5 +1,4 @@
 local set = vim.o
-
 set.number = true
 set.relativenumber = true
 set.clipboard = "unnamed"
@@ -28,3 +27,24 @@ vim.keymap.set("n", "<space>", ":", opt)
 
 vim.keymap.set("n", "<leader>b", "<C-w>s", opt)
 vim.keymap.set("n", "<leader>v", "<C-w>v", opt)
+
+vim.keymap.set("n", "j", [[v:count ? 'j' : 'gj']], { noremap = true, expr = true})
+vim.keymap.set("n", "k", [[v:count ? 'k' : 'gk']], { noremap = true, expr = true})
+
+-- lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  { "catppuccin/nvim", name = "catppuccin", lazy = true}})
+vim.cmd.colorscheme("catppuccin-frappe")
