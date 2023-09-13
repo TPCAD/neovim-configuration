511821be3e26d5c526de65e5796177d31cfb774c
TPCAD <1814083420@qq.com>
2023-09-13 15:10:11 +0800

    python formatter black

--- lua/config/plugins/null-ls.lua
+++ lua/config/plugins/null-ls.lua
@@ -9,6 +9,7 @@ return {
 		null_ls.setup({
 			sources = {
 				null_ls.builtins.formatting.stylua,
+				null_ls.builtins.formatting.black,
 
 				-- c/cpp formater
 				null_ls.builtins.formatting.clang_format,
