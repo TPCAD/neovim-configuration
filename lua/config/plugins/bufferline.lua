cd537d59ed997c7e45db6320dd3d9c12b801b8b9
TPCAD <1814083420@qq.com>
2024-03-22 22:53:51 +0800

    add: add some configurations of bufferline
    1. change sperator style of buffer into slope
    2. add diagnostics indicators in buffer(local buffer)
    3. add diagnostics incicators on the right of bufferline(global)
    
    However, the diagnostics indicators(global) on bufferline seems to have bugs
    that can't render color correctly. Therefore, set the color to
    default.

--- lua/config/plugins/bufferline.lua
+++ lua/config/plugins/bufferline.lua
@@ -10,6 +10,15 @@ return {
   config = function()
     require("bufferline").setup({
       options = {
+        separator_style = "slope", -- slant, slope, thick, thin, { ">", "<" }
+        diagnostics = "nvim_lsp",
+        diagnostics_indicator = function(_, level, _, context)
+          if context.buffer:current() then
+            return ""
+          end
+          local icon = level:match("error") and "" or ""
+          return "" .. icon
+        end,
         offsets = {
           {
             filetype = "NvimTree",
@@ -17,6 +26,29 @@ return {
             text_align = "center",
             separator = true,
           },
+        },
+        custom_areas = {
+          right = function()
+            local result = {}
+            local seve = vim.diagnostic.severity
+            local error = #vim.diagnostic.get(0, { severity = seve.ERROR })
+            local warning = #vim.diagnostic.get(0, { severity = seve.WARN })
+            local hint = #vim.diagnostic.get(0, { severity = seve.HINT })
+
+            if error ~= 0 then
+              table.insert(result, { text = "  " .. error })
+            end
+
+            if warning ~= 0 then
+              table.insert(result, { text = "  " .. warning })
+            end
+
+            if hint ~= 0 then
+              table.insert(result, { text = "  " .. hint })
+            end
+
+            return result
+          end,
         },
       },
     })
