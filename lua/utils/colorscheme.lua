269df620e81dbbb6b4c71ca8b94687ea0a60c88e
TPCAD <1814083420@qq.com>
2024-06-26 14:59:40 +0800

    utils: colorscheme switcher

---
+++ lua/utils/colorscheme.lua
@@ -0,0 +1,11 @@
+-- Start lazy color scheme picker
+local telescope = require("telescope.builtin")
+local function openColorschemePickerWithEvent()
+  -- Trigger the User LazyColorscheme event
+  vim.cmd("doautocmd User LazyColorscheme")
+  -- Then open the colorscheme picker
+  telescope.colorscheme({ enable_preview = true })
+end
+-- Set the keymap to call the function
+vim.keymap.set("n", "<leader>uc", openColorschemePickerWithEvent, { desc = "Switch Colorscheme" })
+-- End lazy color scheme picker
