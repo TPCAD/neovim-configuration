b1e02d0ae861f9f68cb9d62ec474ad73aa2f34b7
TPCAD <1814083420@qq.com>
2024-06-26 13:17:39 +0800

    plugins: which-key

---
+++ lua/plugins/which-key.lua
@@ -0,0 +1,27 @@
+return {
+  "folke/which-key.nvim",
+  keys = {
+    "<leader>",
+    "'",
+    '"',
+    "`",
+    "c",
+    "v",
+    "g",
+    { "<leader>wk", ":WhichKey<CR>", desc = "Which-key All Keymaps" },
+  },
+  init = function()
+    vim.o.timeout = true
+    vim.o.timeoutlen = 300
+  end,
+  opts = {},
+  config = function()
+    local wk = require("which-key")
+    wk.register({
+      ["<leader>w"] = { name = "Which Key" },
+      ["<leader>b"] = { name = "Split Window Below" },
+      ["<leader>v"] = { name = "Split Window Right" },
+      ["<leader>x"] = { name = "Delete Current Buffer" },
+    })
+  end,
+}
