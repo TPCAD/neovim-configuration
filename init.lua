e2b2dd8fe1914f2d88caf9b74502ba8c5cd8ceb0
TPCAD <1814083420@qq.com>
2024-06-30 20:01:03 +0800

    config: config for vscode-neovim
    bad experience

--- init.lua
+++ init.lua
@@ -1,5 +1,9 @@
-require("config.autocmd")
-require("config.basic")
-require("config.keymaps")
-require("config.plugins")
-require("utils.colorscheme")
+if vim.g.vscode then
+  require("config.vscode")
+else
+  require("config.autocmd")
+  require("config.basic")
+  require("config.keymaps")
+  require("config.plugins")
+  require("utils.colorscheme")
+end
