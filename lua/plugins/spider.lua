ed88bdfe842433c8057ed5cefe1e4b0e0a91c1e8
TPCAD <1814083420@qq.com>
2024-11-30 14:53:57 +0800

    plugins: change keymap for sipder.nvim

--- lua/plugins/spider.lua
+++ lua/plugins/spider.lua
@@ -2,17 +2,17 @@ return {
   "chrisgrieser/nvim-spider",
   keys = {
     {
-      "e",
+      "<leader>u",
       "<cmd>lua require('spider').motion('e')<CR>",
       mode = { "n", "o", "x" },
     },
     {
-      "w",
+      "<leader>m",
       "<cmd>lua require('spider').motion('w')<CR>",
       mode = { "n", "o", "x" },
     },
     {
-      "b",
+      "<leader>i",
       "<cmd>lua require('spider').motion('b')<CR>",
       mode = { "n", "o", "x" },
     },
