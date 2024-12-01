0ffc3b6df52b8d1487d7a805f0be12333c9fc1e5
TPCAD <1814083420@qq.com>
2024-12-01 15:04:40 +0800

    keymaps: keymaps options

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,29 +3,35 @@ vim.g.mapleader = ";"
 -- vim.keymap.set({mode}, {lhs}, {rhs}, {opts})
 -- {opts} noremap: disable recursive mapping, desc: description of keymap
 
-local opts = { noremap = true, silent = true }
+-- local opts = { silent = true }
+local function opts(description)
+  return {
+    silent = true,
+    desc = description,
+  }
+end
 
 -- split window
-vim.keymap.set("n", "<leader>b", "<C-w>s", opts)
-vim.keymap.set("n", "<leader>v", "<C-w>v", opts)
+vim.keymap.set("n", "<leader>b", "<C-w>s", opts("Split window honrizontal"))
+vim.keymap.set("n", "<leader>v", "<C-w>v", opts("Split window vertically"))
 
 -- switch window
-vim.keymap.set("n", "<C-h>", "<C-w>h", opts)
-vim.keymap.set("n", "<C-j>", "<C-w>j", opts)
-vim.keymap.set("n", "<C-k>", "<C-w>k", opts)
-vim.keymap.set("n", "<C-l>", "<C-w>l", opts)
+vim.keymap.set("n", "<C-h>", "<C-w>h", opts("Switch to right window"))
+vim.keymap.set("n", "<C-j>", "<C-w>j", opts("Switch to bottom window"))
+vim.keymap.set("n", "<C-k>", "<C-w>k", opts("Switch to up window"))
+vim.keymap.set("n", "<C-l>", "<C-w>l", opts("Switch to left window"))
 
 -- exit insert with jk
-vim.keymap.set("i", "jk", "<esc>", opts)
+vim.keymap.set("i", "jk", "<esc>", opts("Exit insert mode"))
 -- enter command with space
-vim.keymap.set("n", "<space>", ":", opts)
+vim.keymap.set("n", "<space>", ":", opts("Enter command mode"))
 
 -- move cursor up and down more intelligent
-vim.keymap.set("n", "j", [[v:count ? 'j' : 'gj']], { noremap = true, expr = true })
-vim.keymap.set("n", "k", [[v:count ? 'k' : 'gk']], { noremap = true, expr = true })
+vim.keymap.set("n", "j", [[v:count ? 'j' : 'gj']], { expr = true, desc = "move cursor down" })
+vim.keymap.set("n", "k", [[v:count ? 'k' : 'gk']], { expr = true, desc = "move cursor up" })
 
 -- delete buffer
-vim.keymap.set("n", "<leader>x", ":bd<CR>", opts)
+vim.keymap.set("n", "<leader>x", ":bd<CR>", opts("Delete current buffer"))
 
 -- termial mode prefix key
-vim.keymap.set("t", "<leader>jk", "<C-\\><C-n>", opts)
+vim.keymap.set("t", "<leader>jk", "<C-\\><C-n>", opts("Termial mode prefix key"))
