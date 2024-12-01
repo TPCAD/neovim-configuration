8348c4fe4af62c74c638c11bb5ac21b958445d65
TPCAD <1814083420@qq.com>
2024-12-01 21:27:35 +0800

    keymaps: modify description

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -12,29 +12,29 @@ local function opts(description)
 end
 
 -- split window
-vim.keymap.set("n", "<leader>sh", "<C-w>s", opts("Split window horizontally"))
-vim.keymap.set("n", "<leader>sv", "<C-w>v", opts("Split window vertically"))
+vim.keymap.set("n", "<leader>sh", "<C-w>s", opts("Split Window Horizontally"))
+vim.keymap.set("n", "<leader>sv", "<C-w>v", opts("Split Window Vertically"))
 
 -- switch window
-vim.keymap.set("n", "<C-h>", "<C-w>h", opts("Switch to right window"))
-vim.keymap.set("n", "<C-j>", "<C-w>j", opts("Switch to bottom window"))
-vim.keymap.set("n", "<C-k>", "<C-w>k", opts("Switch to up window"))
-vim.keymap.set("n", "<C-l>", "<C-w>l", opts("Switch to left window"))
+vim.keymap.set("n", "<C-h>", "<C-w>h", opts("Switch to Right Window"))
+vim.keymap.set("n", "<C-j>", "<C-w>j", opts("Switch to Lower Window"))
+vim.keymap.set("n", "<C-k>", "<C-w>k", opts("Switch to Upper Window"))
+vim.keymap.set("n", "<C-l>", "<C-w>l", opts("Switch to Left Window"))
 
 -- resize window
-vim.keymap.set("n", "<C-Left>", ":vertical resize -2<CR>", opts("Decrease current window vertically"))
-vim.keymap.set("n", "<C-Right>", ":vertical resize +2<CR>", opts("Increase current window vertically"))
-vim.keymap.set("n", "<C-Up>", ":resize +2<CR>", opts("Increase current window horizontally"))
-vim.keymap.set("n", "<C-Down>", ":resize -2<CR>", opts("Decrease current window horizontally"))
+vim.keymap.set("n", "<C-Left>", ":vertical resize -2<CR>", opts("Decrease Window Width"))
+vim.keymap.set("n", "<C-Right>", ":vertical resize +2<CR>", opts("Increase Window Width"))
+vim.keymap.set("n", "<C-Up>", ":resize +2<CR>", opts("Increase Window Height"))
+vim.keymap.set("n", "<C-Down>", ":resize -2<CR>", opts("Decrease Window Height"))
 
 -- exit insert with jk
-vim.keymap.set("i", "jk", "<esc>", opts("Exit insert mode"))
+vim.keymap.set("i", "jk", "<esc>", opts("Exit Insert Mode"))
 -- enter command with space
-vim.keymap.set("n", "<space>", ":", opts("Enter command mode"))
+vim.keymap.set("n", "<space>", ":", opts("Enter Command Mode"))
 
 -- move cursor up and down more intelligent
-vim.keymap.set("n", "j", [[v:count ? 'j' : 'gj']], { expr = true, desc = "move cursor down" })
-vim.keymap.set("n", "k", [[v:count ? 'k' : 'gk']], { expr = true, desc = "move cursor up" })
+vim.keymap.set("n", "j", [[v:count ? 'j' : 'gj']], { expr = true, desc = "Move Cursor Down" })
+vim.keymap.set("n", "k", [[v:count ? 'k' : 'gk']], { expr = true, desc = "Move Cursor Up" })
 
 -- delete buffer
 vim.keymap.set("n", "<leader>x", ":bd<CR>", opts("Delete current buffer"))
