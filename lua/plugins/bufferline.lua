df3e0f363246d5448143b4a02fe462e845987f3c
TPCAD <1814083420@qq.com>
2024-12-02 16:03:43 +0800

    plugins: bufferline. remove keys field

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -3,10 +3,6 @@ return {
   version = "*",
   event = "BufRead",
   dependencies = { "nvim-tree/nvim-web-devicons" },
-  keys = {
-    { "<Tab>", ":bnext<CR>", desc = "switch to next buffer" },
-    { "<S-Tab>", ":bprevious<CR>", desc = "switch to previous buffer" },
-  },
   config = function()
     require("bufferline").setup({
       options = {
