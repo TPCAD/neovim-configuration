3769abf0001048df2aab7681aec5c51b8eeeadd6
TPCAD <1814083420@qq.com>
2024-12-03 16:40:40 +0800

    plugins: todo-comments. change keymaps

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -1,12 +1,29 @@
 return {
   "folke/todo-comments.nvim",
   dependencies = { "nvim-lua/plenary.nvim" },
-  opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
-  },
+  opts = {},
   keys = {
-    { "<leader>ct", ":Trouble todo<CR>", desc = "Todo comments" },
+    {
+      "]t",
+      function()
+        require("todo-comments").jump_next()
+      end,
+      desc = "Next Todo Comment",
+    },
+    {
+      "[t",
+      function()
+        require("todo-comments").jump_prev()
+      end,
+      desc = "Previous Todo Comment",
+    },
+    { "<leader>xt", "<cmd>Trouble todo<CR>", desc = "Todo (Trouble)" },
+    {
+      "<leader>xT",
+      "<cmd>Trouble todo toggle filter = {tag = {TODO,FIX,FIXME}}<cr>",
+      desc = "Todo/Fix/Fixme (Trouble)",
+    },
+    { "<leader>st", "<cmd>TodoTelescope<cr>", desc = "Todo" },
+    { "<leader>sT", "<cmd>TodoTelescope keywords=TODO,FIX,FIXME<cr>", desc = "Todo/Fix/Fixme" },
   },
 }
