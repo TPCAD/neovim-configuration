0d7fb33c6047a84718e9fc31ef2d7738585ac4df
TPCAD <1814083420@qq.com>
2024-12-03 23:04:19 +0800

    plugins: colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -4,16 +4,18 @@ return {
     name = "catppuccin",
     priority = 1000,
     lazy = false,
-    config = function()
+    opts = function()
       vim.cmd.colorscheme("catppuccin-frappe")
     end,
   },
   {
     "shaunsingh/nord.nvim",
+    lazy = true,
     event = "User LazyColorscheme",
   },
   {
     "loctvl842/monokai-pro.nvim",
+    lazy = true,
     event = "User LazyColorscheme",
   },
 }
