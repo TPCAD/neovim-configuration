0af97d80b36f8d2f9b42f3bc6d8500c78a14d2b6
TPCAD <1814083420@qq.com>
2024-12-03 10:00:33 +0800

    plugins: bufferline. keymaps and opts

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,29 +1,40 @@
 return {
   "akinsho/bufferline.nvim",
   version = "*",
-  event = "BufRead",
+  event = "VeryLazy",
+  keys = {
+    { "<leader>bp", "<Cmd>BufferLineTogglePin<CR>", desc = "Toggle Pin" },
+    { "<leader>bP", "<Cmd>BufferLineGroupClose ungrouped<CR>", desc = "Delete Non-Pinned Buffers" },
+    { "<leader>br", "<Cmd>BufferLineCloseRight<CR>", desc = "Delete Buffers to the Right" },
+    { "<leader>bl", "<Cmd>BufferLineCloseLeft<CR>", desc = "Delete Buffers to the Left" },
+    { "<S-tab>", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev Buffer" },
+    { "<tab>", "<cmd>BufferLineCycleNext<cr>", desc = "Next Buffer" },
+    { "[b", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev Buffer" },
+    { "]b", "<cmd>BufferLineCycleNext<cr>", desc = "Next Buffer" },
+    { "[B", "<cmd>BufferLineMovePrev<cr>", desc = "Move buffer prev" },
+    { "]B", "<cmd>BufferLineMoveNext<cr>", desc = "Move buffer next" },
+  },
   dependencies = { "nvim-tree/nvim-web-devicons" },
-  config = function()
-    require("bufferline").setup({
-      options = {
-        separator_style = "slope", -- slant, slope, thick, thin, { ">", "<" }
-        diagnostics = "nvim_lsp",
-        diagnostics_indicator = function(_, level, _, context)
-          if context.buffer:current() then
-            return ""
-          end
-          local icon = level:match("error") and "" or ""
-          return "" .. icon
-        end,
-        offsets = {
-          {
-            filetype = "NvimTree",
-            text = "File Explorer",
-            text_align = "center",
-            separator = true,
-          },
+  opts = {
+    options = {
+      separator_style = "slope", -- slant, slope, thick, thin, { ">", "<" }
+      always_show_bufferline = false,
+      diagnostics = "nvim_lsp",
+      diagnostics_indicator = function(_, level, _, context)
+        if context.buffer:current() then
+          return ""
+        end
+        local icon = level:match("error") and "" or ""
+        return "" .. icon
+      end,
+      offsets = {
+        {
+          filetype = "NvimTree",
+          text = "File Explorer",
+          text_align = "center",
+          separator = true,
         },
       },
-    })
-  end,
+    },
+  },
 }
