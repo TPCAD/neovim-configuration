1f0272410b7c969770f60b186ded7fa634b6d4d2
TPCAD <1814083420@qq.com>
2024-12-03 10:48:16 +0800

    plugins: nvim-tree. replace `config` with `opts`

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,17 +1,16 @@
 return {
+  -- file explorer
   "nvim-tree/nvim-tree.lua",
   cmd = { "NvimTreeOpen", "NvimTreeToggle" },
   keys = {
     { "<leader>e", ":NvimTreeToggle<CR>", "n", desc = "File Explorer" },
   },
-  config = function()
-    require("nvim-tree").setup({
-      hijack_unnamed_buffer_when_opening = true,
-      disable_netrw = true,
-      hijack_cursor = true,
-      diagnostics = {
-        enable = true,
-      },
-    })
-  end,
+  opts = {
+    hijack_unnamed_buffer_when_opening = true,
+    disable_netrw = true,
+    hijack_cursor = true,
+    diagnostics = {
+      enable = true,
+    },
+  },
 }
