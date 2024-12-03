1ee47523df5fefcc9c052d890f5a837fb19a15b8
TPCAD <1814083420@qq.com>
2024-12-04 00:06:54 +0800

    plugins: spder. keymaps desc

--- lua/plugins/spider.lua
+++ lua/plugins/spider.lua
@@ -5,16 +5,19 @@ return {
       "<leader>u",
       "<cmd>lua require('spider').motion('e')<CR>",
       mode = { "n", "o", "x" },
+      desc = "Spider e",
     },
     {
       "<leader>m",
       "<cmd>lua require('spider').motion('w')<CR>",
       mode = { "n", "o", "x" },
+      desc = "Spider w",
     },
     {
       "<leader>i",
       "<cmd>lua require('spider').motion('b')<CR>",
       mode = { "n", "o", "x" },
+      desc = "Spider b",
     },
     -- ...
   },
