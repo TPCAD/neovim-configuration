e95d62a33306a3c3f782479ad4e71f243f980f26
TPCAD <1814083420@qq.com>
2024-12-03 15:35:50 +0800

    plugins: lualine. add trouble symbols to lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,7 +2,31 @@ return {
   "nvim-lualine/lualine.nvim",
   event = "BufRead",
   dependencies = { "nvim-tree/nvim-web-devicons" },
-  opts = {
-    extensions = { "nvim-tree", "trouble" },
-  },
+  opts = function()
+    local opts = {
+      sections = {
+        lualine_c = { "filename" },
+      },
+      extensions = { "nvim-tree", "trouble" },
+    }
+
+    -- add trouble symbols to lualine
+    local trouble = require("trouble")
+    local symbols = trouble.statusline({
+      mode = "symbols",
+      groups = {},
+      title = false,
+      filter = { range = true },
+      format = "{kind_icon}{symbol.name:Normal}",
+      -- The following line is needed to fix the background color
+      -- Set it to the lualine section you want to use
+      hl_group = "lualine_c_normal",
+    })
+    table.insert(opts.sections.lualine_c, {
+      symbols.get,
+      cond = symbols.has,
+    })
+
+    return opts
+  end,
 }
