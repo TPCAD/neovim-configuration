d5db3c45f6d34a9c35d64fb644a911668f83f60b
TPCAD <1814083420@qq.com>
2024-12-04 18:00:28 +0800

    plugins: nvim-ufo. BufRead event

--- lua/plugins/nvim-ufo.lua
+++ lua/plugins/nvim-ufo.lua
@@ -1,5 +1,6 @@
 return {
   "kevinhwang91/nvim-ufo",
+  event = "BufRead",
   dependencies = { "kevinhwang91/promise-async" },
   config = function()
     vim.o.foldcolumn = "1" -- '0' is not bad vim.foldlevel = 99 vim.o.foldlevelstart = 99
