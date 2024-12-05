c630fa49d76cf416b0de5f11e11d3fdfc570d40a
TPCAD <1814083420@qq.com>
2024-12-05 09:15:32 +0800

    plugins: treesitter. add ensured install parser and enable increment selection

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -6,9 +6,14 @@ return {
   main = "nvim-treesitter.configs",
   opts = {
     ensure_installed = {
+      "bash",
       "c",
+      "cpp",
       "lua",
+      "query",
       "rust",
+      "vim",
+      "vimdoc",
     },
     auto_install = false,
     highlight = {
@@ -16,6 +21,15 @@ return {
     },
     indent = {
       enable = true,
+    },
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = "<leader>nn", -- set to `false` to disable one of the mappings
+        node_incremental = "<leader>nn",
+        scope_incremental = "<leader>ns",
+        node_decremental = "<leader>nm",
+      },
     },
   },
 }
