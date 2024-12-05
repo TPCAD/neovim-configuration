2e1b42ffc83518f6fa981b5826788a08579c9435
TPCAD <1814083420@qq.com>
2024-12-05 22:58:52 +0800

    plugins: noice. keymaps

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -1,6 +1,19 @@
 return {
   "folke/noice.nvim",
   event = "VeryLazy",
+  -- stylua: ignore start
+  keys = {
+    { "<leader>sn", "", desc = "+noice" },
+    { "<leader>snl", function() require("noice").cmd("last") end, desc = "Noice Last Message", },
+    { "<leader>snh", function() require("noice").cmd("history") end, desc = "Noice History" },
+    { "<leader>sna", function() require("noice").cmd("all") end, desc = "Noice All" },
+    { "<leader>snd", function() require("noice").cmd("dismiss") end, desc = "Dismiss All" },
+    { "<leader>snt", function() require("noice").cmd("pick") end, desc = "Noice Picker (Telescope/FzfLua)" },
+    { "<c-f>", function() if not require("noice.lsp").scroll(4) then return "<c-f>" end end, silent = true, expr = true, desc = "Scroll Forward", mode = {"i", "n", "s"} },
+    { "<c-b>", function() if not require("noice.lsp").scroll(-4) then return "<c-b>" end end, silent = true, expr = true, desc = "Scroll Backward", mode = {"i", "n", "s"}},
+  -- stylua: ignore end
+
+  },
   opts = {
     lsp = {
       -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
