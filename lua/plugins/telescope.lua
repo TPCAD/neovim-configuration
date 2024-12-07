b8e57ab6e9d53f78e49af911b195eef7d2fae17d
TPCAD <1814083420@qq.com>
2024-12-07 15:48:50 +0800

    plugins: dressing. better vim.ui with telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,66 +1,85 @@
 -- riggrep required
 return {
-  "nvim-telescope/telescope.nvim",
-  branch = "0.1.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-  },
-  cmd = "Telescope",
-  keys = {
-    { "<leader>,", "<cmd>Telescope buffers<cr>", desc = "Switch Buffers" },
-    { "<leader>/", "<cmd>Telescope live_grep<cr>", desc = "Grep" },
-    { "<leader><space>", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
-    { "<leader>:", "<cmd>Telescope command_history<cr>", desc = "Command History" },
-    -- find
-    { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
-    { "<leader>fb", "<cmd>Telescope buffers ignore_current_buffer=true<cr>", desc = "Buffers" },
-    { "<leader>fg", "<cmd>Telescope git_files<cr>", desc = "Find Files (git-files)" },
-    { "<leader>fr", "<cmd>Telescope oldfiles<cr>", desc = "Recent" },
-    -- git
-    { "<leader>gc", "<cmd>Telescope git_commits<cr>", desc = "Git Commits" },
-    { "<leader>gs", "<cmd>Telescope git_status<cr>", desc = "Git Status" },
-    -- search
-    { "<leader>sb", "<cmd>Telescope current_buffer_fuzzy_find<cr>", desc = "Search in Current Buffer" },
-    { "<leader>sc", "<cmd>Telescope command_history<cr>", desc = "Command History" },
-    { "<leader>sC", "<cmd>Telescope commands<cr>", desc = "Commands" },
-    { "<leader>sd", "<cmd>Telescope diagnostics bufnr=0<cr>", desc = "Document Diagnostics" },
-    { "<leader>sD", "<cmd>Telescope diagnostics<cr>", desc = "Workspace Diagnostics" },
-    { "<leader>sg", "<cmd>Telescope live_grep<cr>", desc = "Grep" },
-    { "<leader>sh", "<cmd>Telescope help_tags<cr>", desc = "Help Pages" },
-    { "<leader>sH", "<cmd>Telescope highlights<cr>", desc = "Search Highlight Groups" },
-    -- { "<leader>sj", "<cmd>Telescope jumplist<cr>", desc = "Jumplist" },
-    { "<leader>sk", "<cmd>Telescope keymaps<cr>", desc = "Key Maps" },
-    { "<leader>sl", "<cmd>Telescope loclist<cr>", desc = "Location List" },
-    { "<leader>sM", "<cmd>Telescope man_pages<cr>", desc = "Man Pages" },
-    { "<leader>sm", "<cmd>Telescope marks<cr>", desc = "Jump to Mark" },
-    { "<leader>so", "<cmd>Telescope vim_options<cr>", desc = "Options" },
-    { "<leader>sR", "<cmd>Telescope resume<cr>", desc = "Resume" },
-    { "<leader>sq", "<cmd>Telescope quickfix<cr>", desc = "Quickfix List" },
-    { "<leader>sw", "<cmd>Telescope grep_string<cr>", desc = "Search Word under Cursor" },
-  },
-  config = function()
-    local open_with_trouble = require("trouble.sources.telescope").open
+  {
+    "nvim-telescope/telescope.nvim",
+    branch = "0.1.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+    },
+    cmd = "Telescope",
+    keys = {
+      { "<leader>,", "<cmd>Telescope buffers<cr>", desc = "Switch Buffers" },
+      { "<leader>/", "<cmd>Telescope live_grep<cr>", desc = "Grep" },
+      { "<leader><space>", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
+      { "<leader>:", "<cmd>Telescope command_history<cr>", desc = "Command History" },
+      -- find
+      { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
+      { "<leader>fb", "<cmd>Telescope buffers ignore_current_buffer=true<cr>", desc = "Buffers" },
+      { "<leader>fg", "<cmd>Telescope git_files<cr>", desc = "Find Files (git-files)" },
+      { "<leader>fr", "<cmd>Telescope oldfiles<cr>", desc = "Recent" },
+      -- git
+      { "<leader>gc", "<cmd>Telescope git_commits<cr>", desc = "Git Commits" },
+      { "<leader>gs", "<cmd>Telescope git_status<cr>", desc = "Git Status" },
+      -- search
+      { "<leader>sb", "<cmd>Telescope current_buffer_fuzzy_find<cr>", desc = "Search in Current Buffer" },
+      { "<leader>sc", "<cmd>Telescope command_history<cr>", desc = "Command History" },
+      { "<leader>sC", "<cmd>Telescope commands<cr>", desc = "Commands" },
+      { "<leader>sd", "<cmd>Telescope diagnostics bufnr=0<cr>", desc = "Document Diagnostics" },
+      { "<leader>sD", "<cmd>Telescope diagnostics<cr>", desc = "Workspace Diagnostics" },
+      { "<leader>sg", "<cmd>Telescope live_grep<cr>", desc = "Grep" },
+      { "<leader>sh", "<cmd>Telescope help_tags<cr>", desc = "Help Pages" },
+      { "<leader>sH", "<cmd>Telescope highlights<cr>", desc = "Search Highlight Groups" },
+      -- { "<leader>sj", "<cmd>Telescope jumplist<cr>", desc = "Jumplist" },
+      { "<leader>sk", "<cmd>Telescope keymaps<cr>", desc = "Key Maps" },
+      { "<leader>sl", "<cmd>Telescope loclist<cr>", desc = "Location List" },
+      { "<leader>sM", "<cmd>Telescope man_pages<cr>", desc = "Man Pages" },
+      { "<leader>sm", "<cmd>Telescope marks<cr>", desc = "Jump to Mark" },
+      { "<leader>so", "<cmd>Telescope vim_options<cr>", desc = "Options" },
+      { "<leader>sR", "<cmd>Telescope resume<cr>", desc = "Resume" },
+      { "<leader>sq", "<cmd>Telescope quickfix<cr>", desc = "Quickfix List" },
+      { "<leader>sw", "<cmd>Telescope grep_string<cr>", desc = "Search Word under Cursor" },
+    },
+    config = function()
+      local open_with_trouble = require("trouble.sources.telescope").open
 
-    -- Use this to add more results without clearing the trouble list
-    -- local add_to_trouble = require("trouble.sources.telescope").add
+      -- Use this to add more results without clearing the trouble list
+      -- local add_to_trouble = require("trouble.sources.telescope").add
 
-    require("telescope").setup({
-      defaults = {
-        mappings = {
-          i = { ["<c-t>"] = open_with_trouble },
-          n = { ["<c-t>"] = open_with_trouble, ["q"] = require("telescope.actions").close },
+      require("telescope").setup({
+        defaults = {
+          mappings = {
+            i = { ["<c-t>"] = open_with_trouble },
+            n = { ["<c-t>"] = open_with_trouble, ["q"] = require("telescope.actions").close },
+          },
         },
-      },
-      extensions = {
-        fzf = {
-          fuzzy = true,
-          override_generic_sorter = true,
-          override_file_sorter = true,
-          case_mode = "smart_case",
+        extensions = {
+          fzf = {
+            fuzzy = true,
+            override_generic_sorter = true,
+            override_file_sorter = true,
+            case_mode = "smart_case",
+          },
         },
-      },
-    })
-    require("telescope").load_extension("fzf")
-  end,
+      })
+      require("telescope").load_extension("fzf")
+    end,
+  },
+  -- better vim.ui with telescope
+  {
+    "stevearc/dressing.nvim",
+    lazy = true,
+    init = function()
+      ---@diagnostic disable-next-line: duplicate-set-field
+      vim.ui.select = function(...)
+        require("lazy").load({ plugins = { "dressing.nvim" } })
+        return vim.ui.select(...)
+      end
+      ---@diagnostic disable-next-line: duplicate-set-field
+      vim.ui.input = function(...)
+        require("lazy").load({ plugins = { "dressing.nvim" } })
+        return vim.ui.input(...)
+      end
+    end,
+  },
 }
