5f18afae3152a89d98bc7c378e72587d7c3c4a93
TPCAD <1814083420@qq.com>
2024-12-20 17:05:31 +0800

    feat(spider): make spider movement as default movement

--- lua/plugins/spider.lua
+++ lua/plugins/spider.lua
@@ -2,23 +2,40 @@ return {
   "chrisgrieser/nvim-spider",
   keys = {
     {
-      "<leader>me",
+      "e",
       "<cmd>lua require('spider').motion('e')<CR>",
       mode = { "n", "o", "x" },
       desc = "Spider e",
     },
     {
-      "<leader>mw",
+      "<leader>me",
+      "e",
+      mode = { "n", "o", "x" },
+      desc = "Normal e",
+    },
+    {
+      "w",
       "<cmd>lua require('spider').motion('w')<CR>",
       mode = { "n", "o", "x" },
       desc = "Spider w",
     },
     {
-      "<leader>mb",
+      "<leader>mw",
+      "w",
+      mode = { "n", "o", "x" },
+      desc = "Normal w",
+    },
+    {
+      "b",
       "<cmd>lua require('spider').motion('b')<CR>",
       mode = { "n", "o", "x" },
       desc = "Spider b",
     },
-    -- ...
+    {
+      "<leader>mb",
+      "b",
+      mode = { "n", "o", "x" },
+      desc = "Normal b",
+    },
   },
 }
