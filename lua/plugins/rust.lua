854cd3e7d80372563ce46d68d5366a0081f55e21
TPCAD <clockcuckoo@outlook.com>
2025-06-08 22:42:26 +0800

    refactor(lsp): replace 'rust-tools.nvim' with 'rustacean.nvim'

--- lua/plugins/rust.lua
+++ lua/plugins/rust.lua
@@ -8,42 +8,10 @@ return {
     end,
   },
 
-  -- rust-tools
+  -- rustacean
   {
-    "simrat39/rust-tools.nvim",
-    ft = "rust",
-    config = function()
-      local on_attach = require("plugins.lsp.utils")
-
-      local opts = {
-        tools = {
-          runnables = {
-            use_telescope = true,
-          },
-          inlay_hints = {
-            auto = true,
-            show_parameter_hints = false,
-            parameter_hints_prefix = "",
-            other_hints_prefix = "",
-          },
-        },
-
-        server = {
-          -- on_attach is a callback called when the language server attachs to the buffer
-          on_attach = on_attach,
-          settings = {
-            -- to enable rust-analyzer settings visit:
-            -- https://github.com/rust-analyzer/rust-analyzer/blob/master/docs/user/generated_config.adoc
-            ["rust-analyzer"] = {
-              -- enable clippy on save
-              checkOnSave = {
-                command = "clippy",
-              },
-            },
-          },
-        },
-      }
-      require("rust-tools").setup(opts)
-    end,
+    "mrcjkb/rustaceanvim",
+    version = "^6", -- Recommended
+    lazy = false, -- This plugin is already lazy
   },
 }
