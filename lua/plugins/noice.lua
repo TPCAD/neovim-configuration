148906952b77e7b99b5d074bd16ece0776e7931f
TPCAD <clockcuckoo@outlook.com>
2025-06-10 19:00:26 +0800

    refactor(lsp): pretty lsp signature help info provided by noice.nvim

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -13,22 +13,43 @@ return {
     { "<c-f>", function() if not require("noice.lsp").scroll(4) then return "<c-f>" end end, silent = true, expr = true, desc = "Scroll Forward", mode = {"i", "n", "s"} },
     { "<c-b>", function() if not require("noice.lsp").scroll(-4) then return "<c-b>" end end, silent = true, expr = true, desc = "Scroll Backward", mode = {"i", "n", "s"}},
   -- stylua: ignore end
-
   },
   opts = {
     lsp = {
+      signature = {
+        enabled = true,
+        opts = {
+          -- relative = "cursor",
+          -- anchor = "NW",
+          position = {
+            row = 2,
+            -- col = 0,
+          },
+          size = {
+            -- width = "auto",
+            -- height = "auto",
+            max_height = 10,
+            max_width = 110,
+          },
+          win_options = {
+            winhighlight = {
+              Normal = "Normal",
+            },
+          },
+        }
+      },
       -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
       override = {
         ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
         ["vim.lsp.util.stylize_markdown"] = true,
-        ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+        -- ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
       },
     },
     presets = {
       bottom_search = true,
       command_palette = true,
       long_message_to_split = true,
-      -- lsp_doc_border = true,
+      lsp_doc_border = true,
     },
     routes = {
       {
@@ -62,13 +83,6 @@ return {
           width = "80%",
           height = "auto"
         }
-      },
-      hover = {
-        win_options = {
-          winhighlight = {
-            Normal = "Normal",
-          },
-        },
       },
     },
   },
