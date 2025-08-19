02d7de2f2114cb6d39b4155a9d9d7a359b6db4b1
TPCAD <clockcuckoo@outlook.com>
2025-08-19 10:10:26 +0800

    refactor(spider): prefix spider key with leader

--- lua/plugins/spider.lua
+++ lua/plugins/spider.lua
@@ -2,40 +2,40 @@ return {
   "chrisgrieser/nvim-spider",
   keys = {
     {
-      "e",
+      "<leader>me",
       "<cmd>lua require('spider').motion('e')<CR>",
       mode = { "n", "o", "x" },
       desc = "Spider e",
     },
+    -- {
+    --   "<leader>me",
+    --   "e",
+    --   mode = { "n", "o", "x" },
+    --   desc = "Normal e",
+    -- },
     {
-      "<leader>me",
-      "e",
-      mode = { "n", "o", "x" },
-      desc = "Normal e",
-    },
-    {
-      "w",
+      "<leader>mw",
       "<cmd>lua require('spider').motion('w')<CR>",
       mode = { "n", "o", "x" },
       desc = "Spider w",
     },
+    -- {
+    --   "<leader>mw",
+    --   "w",
+    --   mode = { "n", "o", "x" },
+    --   desc = "Normal w",
+    -- },
     {
-      "<leader>mw",
-      "w",
-      mode = { "n", "o", "x" },
-      desc = "Normal w",
-    },
-    {
-      "b",
+      "<leader>mb",
       "<cmd>lua require('spider').motion('b')<CR>",
       mode = { "n", "o", "x" },
       desc = "Spider b",
     },
-    {
-      "<leader>mb",
-      "b",
-      mode = { "n", "o", "x" },
-      desc = "Normal b",
-    },
+    -- {
+    --   "<leader>mb",
+    --   "b",
+    --   mode = { "n", "o", "x" },
+    --   desc = "Normal b",
+    -- },
   },
 }
