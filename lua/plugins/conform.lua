11163eb32dd9812452af0f78cc1ff3d085c89615
TPCAD <clockcuckoo@outlook.com>
2025-08-19 10:11:02 +0800

    feat(format): format json with clang_format

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -8,6 +8,7 @@ return {
       c = { "clang_format" },
       lua = { "stylua" },
       sh = { "shfmt" },
+      json = { "clang_format" },
     },
     format_on_save = { timeout_ms = 500, lsp_format = "fallback" },
   },
