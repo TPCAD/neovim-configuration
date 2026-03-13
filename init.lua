fc3cc75dfd40a65b22ed5de2a02538ddadfcaf3a
TPCAD <clockcuckoo@outlook.com>
2026-03-13 16:22:47 +0800

    chore: remove lua prefix in require statement

--- init.lua
+++ init.lua
@@ -1,12 +1,13 @@
 if not vim.g.vscode then
-    require("utils")
-    require("autocmd")
-    require("options")
-    require("keymaps")
-    require("lsp")
-    require("pluginmanager")
-    require("config.terminal")
+  require("utils")
+  require("autocmd")
+  require("options")
+  require("keymaps")
+  require("lsp")
+  require("pluginmanager")
+  require("config.terminal")
 else
-    require("lua.vscodeonly")
-    require("pluginmanager")
+  require("vscodeonly")
+  require("pluginmanager")
 end
+
