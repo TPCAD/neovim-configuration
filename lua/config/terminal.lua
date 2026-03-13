513f453537d311c0e85fe1ac939e27ffda611121
TPCAD <clockcuckoo@outlook.com>
2026-03-13 16:22:04 +0800

    fix: open lazygit or yazi would remove normal terminal buffer
    Close lazygit or yazi window will trigger TermClose event which will
    reset vim.g.term_bufnr.

--- lua/config/terminal.lua
+++ lua/config/terminal.lua
@@ -1,6 +1,17 @@
+---@type integer
 vim.g.term_bufnr = 0
 
 local toggle_terminal_buffer = function()
+  -- delete existing terminal when first open nvim
+  if vim.g.term_bufnr == 0 then
+    local buf_list = vim.api.nvim_list_bufs()
+    for _, bufnr in ipairs(buf_list) do
+      if string.find(vim.fn.bufname(bufnr), "^term://.*") then
+        vim.api.nvim_buf_delete(bufnr, { force = true, unload = false })
+      end
+    end
+  end
+
   -- switch to alternate buf if terminal buffer is normal mode
   if vim.g.term_bufnr == vim.api.nvim_win_get_buf(0) then
     vim.cmd("b#")
@@ -8,7 +19,7 @@ local toggle_terminal_buffer = function()
   end
 
   -- switch to terminal buffer if exist
-  if vim.g.term_bufnr ~= 0 then
+  if vim.g.term_bufnr ~= 0 and vim.api.nvim_buf_is_valid(vim.g.term_bufnr) then
     vim.api.nvim_win_set_buf(0, vim.g.term_bufnr)
     vim.cmd("startinsert")
     return
@@ -21,28 +32,20 @@ local toggle_terminal_buffer = function()
   vim.g.term_bufnr = vim.api.nvim_win_get_buf(0)
   vim.bo[vim.g.term_bufnr].filetype = "terminal" -- name terminal bufffer
   vim.bo[vim.g.term_bufnr].buflisted = false -- hide terminal buffer
-  vim.api.nvim_create_autocmd("TermEnter", { -- set terminal buffer
-    group = vim.api.nvim_create_augroup("CustomTerminal", { clear = true }),
-    callback = function()
-      vim.opt_local.number = false
-      vim.opt_local.relativenumber = false
-      vim.opt_local.numberwidth = 1
-    end,
-  })
-
-  vim.api.nvim_create_autocmd("TermClose", { -- set global varibale when kill terminal buffer
-    group = vim.api.nvim_create_augroup("CustomTerminal", { clear = false }),
-    callback = function()
-      vim.g.term_bufnr = 0
-    end,
-  })
 end
 
--- keymap for terminal buffer
-vim.keymap.set("n", "<C-_>", toggle_terminal_buffer, { desc = "Toggle Terminal Buffer" })
-vim.keymap.set("n", "<C-/>", toggle_terminal_buffer, { desc = "Toggle Terminal Buffer" })
+-- configure terminal buffer
+vim.api.nvim_create_autocmd("TermEnter", {
+  group = vim.api.nvim_create_augroup("CustomTerminal", { clear = true }),
+  callback = function()
+    vim.opt_local.number = false
+    vim.opt_local.relativenumber = false
+    vim.opt_local.numberwidth = 1
+  end,
+})
 
-vim.keymap.set("t", "<C-_>", "<cmd>b#<cr>", { desc = "Toggle Terminal Buffer" })
-vim.keymap.set("t", "<C-/>", "<cmd>b#<cr>", { desc = "Toggle Terminal Buffer" })
+-- keymap for terminal buffer
+vim.keymap.set({ "n", "t" }, "<C-_>", toggle_terminal_buffer, { desc = "Toggle Terminal Buffer" })
+vim.keymap.set({ "n", "t" }, "<C-/>", toggle_terminal_buffer, { desc = "Toggle Terminal Buffer" })
 
 vim.keymap.set("t", "<esc><esc>", "<C-\\><C-n>", { desc = "Enter Normal Mode" })
