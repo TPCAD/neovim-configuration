3f08bf6f5743bb3c91b11117b2d26f92580446d5
TPCAD <clockcuckooo@gmail.com>
2026-04-09 10:35:56 +0800

    health: check wsl and clipboard

--- lua/utils/health.lua
+++ lua/utils/health.lua
@@ -1,3 +1,9 @@
+-- 1. check version >= 0.12
+-- 2. check clipboard
+-- 3. check is wsl
+
+local utils = require("utils")
+
 local check_version = function()
   local verstr = tostring(vim.version())
   if not vim.version.ge then
@@ -6,21 +12,43 @@ local check_version = function()
   end
 
   if vim.version.ge(vim.version(), "0.12") then
-    vim.health.ok(string.format("Neovim version is: '%s'", verstr))
+    vim.health.ok(string.format("Neovim version: '%s'", verstr))
   else
-    vim.health.error(string.format("Neovim out of date: '%s'. Neovim 0.11+ is needed", verstr))
+    vim.health.error(string.format("Neovim out of date: '%s'. Neovim 0.12+ is needed", verstr))
+  end
+end
+
+local check_clipboard = function()
+  if utils.clipboard == "" then
+    vim.health.error("No clipboard found")
+    return
+  end
+
+  if utils.is_wsl and utils.clipboard ~= "win32yank" then
+    vim.health.warn(string.format("Found %s", utils.clipboard), [[It's recommanded to use win32yank in WSL]])
+  end
+end
+
+local function check_wsl()
+  if utils.is_wsl then
+    vim.health.ok([[WSL is detected and supported]])
   end
 end
 
 return {
   check = function()
-    vim.health.start("Custom")
-
+    vim.health.start("Dioptra.Version")
     vim.health.info([[Neovim 0.12+ is needed.]])
+    check_version()
+
+    vim.health.start("Dioptra.WSL")
+    check_wsl()
 
     local uv = vim.uv or vim.loop
-    vim.health.info("System Information: " .. vim.inspect(uv.os_uname()))
+    vim.health.info("System Info: " .. vim.inspect(uv.os_uname()))
 
-    check_version()
+
+    vim.health.start("Dioptra.Clipboard")
+    check_clipboard()
   end,
 }
