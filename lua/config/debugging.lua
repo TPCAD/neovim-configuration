61db4faf61d27f250559d51a0fc3520e0b1f24b7
TPCAD <clockcuckooo@gmail.com>
2026-04-23 10:24:10 +0800

    debugging: debug python in cwd

--- lua/config/debugging.lua
+++ lua/config/debugging.lua
@@ -97,6 +97,34 @@ dap.configurations.lua = {
 local path = "~/.local/share/nvim/mason/packages/debugpy/venv/bin/python"
 require("dap-python").setup(path)
 
+dap.configurations.python = dap.configurations.python or {}
+vim.list_extend(dap.configurations.python, {
+  {
+    type = "python",
+    request = "launch",
+    name = "file:args (cwd)",
+    program = "${file}",
+    args = function()
+      local args_string = vim.fn.input("Arguments: ")
+      local utils = require("dap.utils")
+      if utils.splitstr and vim.fn.has("nvim-0.10") == 1 then
+        return utils.splitstr(args_string)
+      end
+      return vim.split(args_string, " +")
+    end,
+    console = "integratedTerminal",
+    cwd = vim.fn.getcwd(),
+  },
+  {
+    type = "python",
+    request = "launch",
+    name = "file: (cwd)",
+    program = "${file}",
+    console = "integratedTerminal",
+    cwd = vim.fn.getcwd(),
+  },
+})
+
 -- UI for nvim-dap
 require("dap-view").setup({
   winbar = {
